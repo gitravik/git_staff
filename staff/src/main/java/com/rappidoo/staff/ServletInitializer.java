1057ef887acfc9c1fa5b3df46acc26a37e275f29
ravi <ravi.kathiriya@peaas.co>
2018-01-11 19:29:26 +0530

    staff demo project

---
+++ staff/src/main/java/com/rappidoo/staff/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rappidoo.staff;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StaffApplication.class);
+	}
+
+}
